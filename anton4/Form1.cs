44051c7322ea89d8119394d2bb14647245926155
res1nx <38017776+res1nx@users.noreply.github.com>
2018-04-08 22:55:08 +0300

    в

--- anton4/Form1.cs
+++ anton4/Form1.cs
@@ -16,5 +16,20 @@ namespace anton4
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double n = Convert.ToDouble(textBox1.Text);
+            double k = Convert.ToDouble(textBox2.Text);
+            double i = 0;
+
+            while (n >= k)
+            {
+                i = i + 1;
+                n = n - k;
+            }
+            double o = n;
+            textBox3.Text += "Делимое = " + i + " остаток " + n + Environment.NewLine;
+        }
     }
 }
